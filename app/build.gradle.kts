28ac86739d8e3ac299081eff47ba01c7c59a2efc
juan.goez <juan.goez@coraltech.co>
2026-05-08 21:03:39 -0500

    Downgrade SDK to API 35 to fix IDE Live Edit compatibility error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.afelix.rifaapp"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.afelix.rifaapp"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
