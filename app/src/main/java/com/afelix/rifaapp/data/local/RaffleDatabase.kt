7003c5850056d3d6c1a30bdeb493a6e187a1dc4f
juan.goez <juan.goez@coraltech.co>
2026-05-16 15:32:27 -0500

    Fix login crash: Bump Room database version to 4 to handle schema changes

--- app/src/main/java/com/afelix/rifaapp/data/local/RaffleDatabase.kt
+++ app/src/main/java/com/afelix/rifaapp/data/local/RaffleDatabase.kt
@@ -3,7 +3,7 @@ package com.afelix.rifaapp.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [RaffleEntity::class, TicketEntity::class], version = 3)
+@Database(entities = [RaffleEntity::class, TicketEntity::class], version = 4)
 abstract class RaffleDatabase : RoomDatabase() {
     abstract val raffleDao: RaffleDao
 }
