b64323648bf258df78b11c0075392350fce1455c
juan.goez <juan.goez@coraltech.co>
2026-06-11 18:38:13 -0500

    Final prep for Play Store: Enabled minification, debug symbols, and bumped version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,15 +13,17 @@ android {
         applicationId = "com.afelix.rifaapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            ndk.debugSymbolLevel = "full" // Añadido para eliminar el aviso de Google Play
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
